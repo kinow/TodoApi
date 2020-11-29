3e607acf7a6531f4d682b44d530167b717f74566
Bruno Kinoshita <kinow@users.noreply.github.com>
2020-11-29 19:17:52 +1300

    Add an DTO for the TodoItem class

---
+++ TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    /// <summary>
+    /// DTO for the <see cref="TodoItem"/> model.
+    /// </summary>
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { set; get; }
+    }
+}

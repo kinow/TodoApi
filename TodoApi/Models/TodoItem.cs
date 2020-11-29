ab01b716f8c86966d0130fb87729e34ae673f837
Bruno Kinoshita <kinow@users.noreply.github.com>
2020-11-29 17:56:03 +1300

    Add TodoItem models class

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        /// <summary>
+        /// The Id property functions as the unique key in a relational database.
+        /// </summary>
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
